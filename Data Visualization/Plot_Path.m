74eff54f7d3b821f7aa584e68076de1128e2157a
Tinker Twins <tinkertwinsofficial@gmail.com>
2023-02-02 21:41:37 -0500

    Create Plot_Path.m

---
+++ Data Visualization/Plot_Path.m
@@ -0,0 +1,22 @@
+%% Plot Vehicle Path from Recorded Data
+
+%% Clear Workspace
+close all;
+clear;
+clc;
+
+%% Import Data
+[mat_files,path] = uigetfile('*.mat','multiselect','off');
+load(fullfile(path,mat_files));
+
+%% Plot Vehicle Path
+figure('Name','Vehicle Path','NumberTitle','off');
+plot(states(:,1),states(:,2),'Color','black')
+hold on
+plot(states(1,1),states(1,2),'o','MarkerSize',5,'MarkerEdgeColor','red','MarkerFaceColor',[1 0.6 0.6])
+plot(states(end,1),states(end,2),'o','MarkerSize',5,'MarkerEdgeColor','blue','MarkerFaceColor',[0.6 0.6 1])
+hold off
+title('Vehicle Path')
+legend('Path','Start','Finish','Location','NE')
+xlabel('Global X-Coordinate (m)')
+ylabel('Global Y-Coordinate (m)')
