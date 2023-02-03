431638acfe98a63b212a7aed6c4dcfd6fc17d08f
Tinker Twins <tinkertwinsofficial@gmail.com>
2023-02-02 22:06:46 -0500

    Add vehicle path plots

--- Data Visualization/Plot_Path.m
+++ Data Visualization/Plot_Path.m
@@ -16,6 +16,9 @@ hold on
 plot(states(1,1),states(1,2),'o','MarkerSize',5,'MarkerEdgeColor','red','MarkerFaceColor',[1 0.6 0.6])
 plot(states(end,1),states(end,2),'o','MarkerSize',5,'MarkerEdgeColor','blue','MarkerFaceColor',[0.6 0.6 1])
 hold off
+xlim([-3.5,2])
+ylim([-3,2.5])
+pbaspect([1 1 1])
 title('Vehicle Path')
 legend('Path','Start','Finish','Location','NE')
 xlabel('Global X-Coordinate (m)')
